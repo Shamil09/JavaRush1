41c9600f6eb9b962440646b7a340b31cfef42175
Шамиль <shama_bid@mail.ru>
2019-02-14 01:10:19 +0300

    3

--- 2.JavaCore/src/com/javarush/task/task17/task1710/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task17/task1710/Solution.java
@@ -1,5 +1,6 @@
 package com.javarush.task.task17.task1710;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
@@ -22,17 +23,29 @@ public class Solution {
 //-d - производит логическое удаление человека с id, заменяет все его данные на null
 //-i - выводит на экран информацию о человеке с id: name sex (м/ж) bd (формат 15-Apr-1990)
     //-c name sex bd
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
         //start here - начни тут
         switch (args[0]) {
             case "-c": {//-c name sex bd
-                if (args[2].equals("м"))allPeople.add(Person.createMale(args[1], new Date(args[3])));
-                if (args[2].equals("ж"))allPeople.add(Person.createFemale(args[1], new Date(args[3])));
+                Date date=new SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH).parse(args[3]);
+                if (args[2].equals("м"))allPeople.add(Person.createMale(args[1], date));
+                if (args[2].equals("ж"))allPeople.add(Person.createFemale(args[1], date));
+                System.out.println(allPeople.size()-1);
                 break;
             }
             case "-u": {//-u id name sex bd
                 int id=Integer.parseInt(args[1]);
                 if (id>=0 && id<allPeople.size()) {
+                    Person p=allPeople.get(id);
+                    p.setName(args[2]);
+                    p.setSex(args[3].equals("м")?Sex.MALE:Sex.FEMALE);
+                    p.setBirthDate(new SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH).parse(args[4]));
+                }
+                break;
+            }
+            case "-d":{//-d id
+                int id=Integer.parseInt(args[1]);
+                if (id>=0 && id<allPeople.size()){
                     Person p=allPeople.get(id);
                     p.setName(null);
                     p.setSex(null);
@@ -40,14 +53,9 @@ public class Solution {
                 }
                 break;
             }
-            case "-d":{//-d id
-                int id=Integer.parseInt(args[1]);
-                if (id>=0 && id<allPeople.size()) allPeople.remove(id);
-                break;
-            }
             case "-i":{
                 Person p=allPeople.get(Integer.parseInt(args[1]));
-                SimpleDateFormat dF = new SimpleDateFormat("dd-MMMM-yyyy",Locale.ENGLISH);
+                SimpleDateFormat dF = new SimpleDateFormat("dd-MMM-yyyy",Locale.ENGLISH);
                 String s=p.getSex().equals(Sex.FEMALE)?"ж":"м";
                 System.out.println(p.getName()+" "+s+" "+dF.format(p.getBirthDate()));
                 break;
