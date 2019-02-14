75201d0c85fd30bdd5fc5f2cdcc3fc6ef1ddaa78
Шамиль <shama_bid@mail.ru>
2019-02-14 18:59:01 +0300

    3

--- 2.JavaCore/src/com/javarush/task/task17/task1712/Cook.java
+++ 2.JavaCore/src/com/javarush/task/task17/task1712/Cook.java
@@ -34,6 +34,6 @@ public class Cook implements Runnable {
         Thread.sleep(order.getTime());     // готовим блюдо
         Dishes dishes = new Dishes(order.getTableNumber());       //  это готовое блюдо
         System.out.println(String.format("Заказ для стола №%d готов", dishes.getTableNumber()));
-        manager.getDishesQueue().add(dishes);
+        manager.getDishesQueue().add(dishes);//это и есть исправление
     }
 }
