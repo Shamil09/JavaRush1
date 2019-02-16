0c5445983ee4afb9e880b105a054838a657a765b
Шамиль <shama_bid@mail.ru>
2019-02-16 03:23:22 +0300

    3

---
+++ 2.JavaCore/src/com/javarush/task/task17/task1721/Solution.java
@@ -0,0 +1,48 @@
+package com.javarush.task.task17.task1721;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+/*
+Транзакционность
+*/
+
+public class Solution {
+    public static List<String> allLines = new ArrayList<String>();
+    public static List<String> forRemoveLines = new ArrayList<String>();
+
+    public static void main(String[] args) throws IOException {
+//        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+//        allLines = Files.readAllLines(Paths.get(reader.readLine()));//Считать все строки из файла
+//        forRemoveLines = Files.readAllLines(Paths.get(reader.readLine()));
+//        reader.close();
+
+        /*Код для теста*/
+        allLines.addAll(Arrays.asList("a", "b","c","d"));
+        forRemoveLines.addAll(Arrays.asList("a", "b","c","d","e","f","g"));
+
+        Solution solution=new Solution();
+        solution.joinData();
+
+        /*Код для теста*/
+        System.out.println(allLines);
+        System.out.println(forRemoveLines);
+    }
+//3. Если список allLines содержит все строки из forRemoveLines, то удалить из списка allLines все строки,
+// которые есть в forRemoveLines.
+//4. Если условие из п.3 не выполнено, то:
+//4.1. очистить allLines от данных
+//4.2. выбросить исключение CorruptedDataException
+//Метод joinData должен вызываться в main. Все исключения обработайте в методе main.
+//Не забудь закрыть потоки.
+    public void joinData() throws CorruptedDataException {
+        if (forRemoveLines.containsAll(allLines)) allLines.removeAll(forRemoveLines);
+        else {
+            allLines.clear();
+            throw new  CorruptedDataException();
+        }
+    }
+}
